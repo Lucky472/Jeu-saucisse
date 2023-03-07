038a5a37aa3493c22308cea69ec2940a6e4a905a
KikiTheLord <82971844+KikiTheLord@users.noreply.github.com>
2023-03-07 20:08:48 +0100

    PSEUDO CODE DE FOU

--- Jeu.py
+++ Jeu.py
@@ -7,37 +7,122 @@ Created on Tue Mar  7 08:08:32 2023
 """
 from tkinter import *
 
-widthCanvas = 800
-heightCanvas = 800
-colorCanvas = "#000000"
+RADIUS = 5
+XMIN = 20
+YMIN = 20
+DIST = 10
+WIDTH = 2*XMIN + 8*DIST
+HEIGHT = 2*YMIN + 6*DIST
+WIDTHCANVAS = 800
+HEIGHTCANVAS = 800
+COLORCANVAS = "#000000"
 
 
 
 class GameShow:
     def __init__(self,window):
+        self.game_engine = GameEngine()
+        #BIND LE CLIC 
+
+
         #Initialise l'interface graphique
         self.window = window
         self.plateau = Frame()
         self.menu = Frame()
-        self.canvas = Canvas(self.plateau, width = widthCanvas,height =heightCanvas,bg=colorCanvas)
-        self.activePlayer = "UI"
-        self.labelActivePlayer = Label(self.menu,textvariable= self.activePlayer)
+        self.canvas = Canvas(self.plateau, width = WIDTHCANVAS,height =HEIGHTCANVAS,bg=COLORCANVAS)
         
-        
+        self.labelActivePlayer = Label(self.menu,textvariable= self.game_engine.activePlayer)
         #Pack l'interface graphique
         self.plateau.pack(expand=YES,side=LEFT)
         self.menu.pack(expand=YES,side=RIGHT)
         self.canvas.pack(expand=YES)
         self.labelActivePlayer.pack(expand=YES)
 
-        self.listPoint =  []
-  
+    def draw_board(self):
+        #parcours la liste et quand il y a un point il le dessine 
+        pass
+
+    def on_click(self,evt):
+        #gère si le click est sur un point et appelle les fonctions associées
+        self.game_engine.on_click(evt)
+        pass
     
+    def draw_sausage(self,points):
+        #dessine une saucisse étant donné un tuple de 3 points
+        pass
+    
+    def highlight_points(self):
+        #met en surbrillance les points accessibles depuis un point sélectionné
+        pass
+    
+    
+    
+
 
 class GameEngine:
     def __init__(self):
-        self.temp = "temp"
-        self.temp2 = "adrien teste un truc"
+        self.board = self.set_new_board()
+        self.activePlayer = "UI"
+
+
+    def on_click(self,evt):
+        # Adrien l'a dans sa tête
+        pass
+
+    def set_new_board(self):
+        #Créer le tableau 2D avec des points en i+j pair et crossing sinon
+        pass
+    
+    def game_over_test(self):
+        #teste si des coups sont encore possibles sur le plateau
+        pass
+    
+    def update_degree(self,point):
+        #calcule le degré ( points libres atteignables) autour du point
+        pass
+    
+    def update_all_degree(self):
+        #update degree pour chaque point 
+        pass
+    
+
+
+    def check_coord_mouse(self,evt):
+        #vérifie si la souris clique sur un point
+        pass
+    
+    
+class Point:
+    def __init__(self):
+        self.occupied = False
+        self.degree
+        self.id
+
+        #ADRIEN A L'IDEE, TABLEAU COORDONNES DE POINTS A DOUBLE ENTREE JE SAIS PLUS QUOI 
+        pass
+    
+
+class Crossing:
+    def __init__(self):
+        self.occupied = False
+
+
+
+
+
+        
+
+
+
+root = Tk()
+root.title("SaucisseTheGame")
+game = GameShow(root)
+
+root.mainloop()
+
+
+
+
 
 """Jeu"""        #Etats : en cours, fini
         #self.activePlayer
@@ -55,10 +140,3 @@ class GameEngine:
         
 """Saucisse"""   #Forme = Trait
         #2 couleurs , 1 pour chaque joueur
-
-
-root = Tk()
-root.title("SaucisseTheGame")
-game = GameShow(root)
-
-root.mainloop()
